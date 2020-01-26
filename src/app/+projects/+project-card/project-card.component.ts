32509429b8ffdbe97b90ecb66eb407003036b473
Chad Hollenbeck <chad.hollenbeck@owlcomputing.com>
2020-01-26 14:21:14 -0500

    updated left nav styles and project status

--- src/app/+projects/+project-card/project-card.component.ts
+++ src/app/+projects/+project-card/project-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { AppService } from '@app/app.service';
 import { Subscription } from 'rxjs';
 import { ProjectVM } from '../models/project.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-project-card',
@@ -18,14 +19,12 @@ export class ProjectCardComponent implements OnInit, OnDestroy {
 
   isRTL = false;
 
+  status: string;
+  statusClass: string;
+
   constructor(private appService: AppService) {
     this.appService.pageTitle = 'Project list - Pages';
   }
-
-  statuses = {
-    1: { title: 'Active', color: 'success' },
-    2: { title: 'Pending', color: 'warning' }
-  };
 
   completedPercent(tasks: number, completed: number) {
     return 100 - Math.round((tasks / completed) * 100);
@@ -34,7 +33,30 @@ export class ProjectCardComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.loading = true;
 
-    // Load Data
+    // Set status information
+    const today = moment();
+    const lastUpdated = moment(this.project.updateDate);
+    const timeSinceUpdate = today.diff(lastUpdated, 'd');
+
+    if(timeSinceUpdate > 60){
+      this.status = this.project.isComplete ? "Complete" : "Archived";
+      this.statusClass = "secondary"
+    }
+
+    if(timeSinceUpdate > 45){
+      this.status = "On Hold";
+      this.statusClass = "warning"
+    }
+
+    if(timeSinceUpdate > 30){
+      this.status = "In Progress";
+      this.statusClass = "info"
+    }
+
+    if(timeSinceUpdate >= 0){
+      this.status = 'Active';
+      this.statusClass= 'success';
+    }
   }
 
   ngOnDestroy() {
--- src/app/+projects/models/project.model.ts
+++ src/app/+projects/models/project.model.ts
@@ -1,12 +1,17 @@
 export class ProjectVM {
   id: string;
-  title: string;
-  tasks: number;
-  completedTasks: number;
-  shortDescription: string;
-  created: string;
-  deadline: string;
-  status: string;
+
+  createDate: number;
+  description: string;
+  isComplete: boolean;
+  git_url: string;
+  html_url: string;
+  language: string;
+  name: string;
+  openIssues: number;
+  size: number;
+  updateDate: string;
+
 
   constructor(){
     this.id = null;
